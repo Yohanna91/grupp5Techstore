e76b47a803bef57a27ac59fda799f2fe3555690d
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2022-11-04 16:39:13 +0100

    Finally again

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
  
  * Vi har en fixerad header med en titel och en kundvagn som är klickbar.
  * Kundvagnen visar rätt siffra beroende på antal produkter i densamma.
- * Startsidan visar alla produkterna och dem tar upp ungefär hela höjden av skärmen tillsammans med             produktinfo.
- * Det finns även för varje produkt en knapp för att lägga till produkterna i kundvagnen och dem sparas även   i LocalStorage.
+ * Startsidan visar alla produkterna och dem tar upp ungefär hela höjden av skärmen tillsammans med produktinfo.
+ * Det finns även för varje produkt en knapp för att lägga till produkterna i kundvagnen och dem sparas även i LocalStorage.
  * Kundvagnsidan listar dem produkterna vi har lagt till i horisontellt centrerat läge.
  (Hämtat det som finns i localStorage och konverterat det från string till JS-objekt)
  * Till varje produkt finns det titel, pris och en ta-bort knapp.
@@ -13,7 +13,14 @@
  * Hemsidan har vi gjort responsiv och den efterliknar bilderna i mockup-mappen.
  * Vi har arbetat enligt GIT-Flow och även låst main branchen så att man inte har kunnat pusha upp direkt.
  
- Vi satsar på G
+ Vi satsar på VG
+
+ * Vi har valt att implementera inloggningsfunktion så att användaren kan se sina ordrar efter att ha bekräftat dem.
+ * Kundvagnen töms när du bekräftar ett köp.
+ * När man loggar in som användare så kan man se en översikt av den nuvarande ordern som skapats.
+ * Vi har designat webshopen men har haft mest fokus på funktionaliteten.
+ * Vi har valt att lägga inloggninsfunktion i mitten på headern för att den ska vara mer lättillgänglig och tydlig.
+ 
  
  
  Länk till repot: https://github.com/Yohanna91/grupp5Techstore.git
--- VGpart..js
+++ VGpart..js
@@ -118,59 +118,6 @@ function uppdatera_skärmen() {
         registrera.style.display = "block"
     }
 }
-
-        //KNYTA IHOP EN ORDER MED EN SPECIFIK ANVÄNDARE
-
-        // function MyPageView() {
-        //     const inloggad = JSON.parse(localStorage.getItem("inloggad"));
-        //     const orders = JSON.parse(localStorage.getItem("orders"));
-        //     const myOrders = orders.filter(order => order.user == inloggad.namn)
-        //     if (myOrdersLocalStorage) {
-        //       let total = 0;
-        //       myOrdersLocalStorage.forEach((order) => {
-        //         if (order.user == inloggad.namn) {
-        //           return;
-        //         }
-        //         total += item.price;
-        //       });
-        //       const orderedByUser = myOrdersLocalStorage[myOrdersLocalStorage.length - 1].user;
-        //       if (orderedByUser == inloggad.namn) {
-        //         //yohanna fredrik etc...är = inloggad.namn
-        //         itemsTotal.innerText = myItemsLocalStorage.length - 1;
-        //         itemsPrice.innerText = total + ":-";
-        //       } else {
-        //         itemsTotal.innerText = "0";
-        //         itemsPrice.innerText = "0:-";
-        //       }
-
-        // const Yohanna = document.getElementById("jonas")
-
-        // Yohanna.addEventListener("click", function() {
-
-        //     console.log("hej")
-            
-            
-        // })
-            
-            // // for (const order of orders) {
-            // //     const ul = document.createElement("ul");
-            // //     const li = document.createElement("li");
-            // //     li.innerText = inloggad.namn
-            // //     ul.appendChild(li)
-            // // }
-            
-            // // for ( const hero of heroes) {
-            // //     if ( hero.teamId === team.teamId) {
-            // //         const heroUL = document.createElement("ul")
-            // //         const heroLI = document.createElement("li")
-            // //         heroLI.innerText = hero.heroName
-            // //         heroUL.append(heroLI)
-            // //         teamUL.appendChild(heroUL)
-                    
-            // //     }
-            // // }
-            
-            // // document.body.appendChild(ul)
         
          
         
